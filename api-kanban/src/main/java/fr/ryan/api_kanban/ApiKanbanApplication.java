7a4896d761b7826bd2f84cc5105fe43bbdbcf7ad
ryan-ladmia <ryan.ladmia@laplateforme.io>
2026-09-22 15:47:44 +0200

    Initialisation and first commit

---
+++ api-kanban/src/main/java/fr/ryan/api_kanban/ApiKanbanApplication.java
@@ -0,0 +1,13 @@
+package fr.ryan.api_kanban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKanbanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiKanbanApplication.class, args);
+	}
+
+}
---
+++ api-kanban/src/test/java/fr/ryan/api_kanban/ApiKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ryan.api_kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
