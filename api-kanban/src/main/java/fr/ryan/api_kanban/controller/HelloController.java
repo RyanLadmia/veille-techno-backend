15ec874caf2d6665e1bf655cc0bbfccd8d7dd306
ryan-ladmia <ryan.ladmia@laplateforme.io>
2026-09-25 12:47:48 +0200

    Ajusting Docker set up

--- api-kanban/src/main/java/fr/ryan/api_kanban/controller/HelloController.java
+++ api-kanban/src/main/java/fr/ryan/api_kanban/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, JAVA!";
+        return "Hello, JAVA not Script!";
     }
 }
